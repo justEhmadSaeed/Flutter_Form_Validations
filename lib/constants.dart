1b59abec700079d643aa14417084828c3fa9f026
Ehmad <justehmadsaeed@gmail.com>
2021-05-25 08:22:41 +0500

    📦 NEW: Validate TextFields

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+final kHeadingTextStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+final kTextFieldStyle = TextStyle(fontSize: 20);
+final klabelStyle = TextStyle(fontSize: 20);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:form_validation/constants.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,104 +11,132 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Form Validation"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: AppBody(),
+    );
+  }
+}
+
+class AppBody extends StatefulWidget {
+  @override
+  _AppBodyState createState() => _AppBodyState();
+}
+
+class _AppBodyState extends State<AppBody> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Form(
+        key: _formKey,
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Register',
+                style: kHeadingTextStyle,
+              ),
+              Text(
+                'Signup to experience new ways',
+                style: TextStyle(color: Colors.grey, fontSize: 20),
+              ),
+              TextFormField(
+                style: kTextFieldStyle,
+                decoration: InputDecoration(
+                    labelText: 'Username',
+                    labelStyle: klabelStyle,
+                    prefixIcon: Icon(Icons.account_circle)),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please Enter Some Text';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                style: kTextFieldStyle,
+                decoration: InputDecoration(
+                    labelText: 'Email Address',
+                    labelStyle: klabelStyle,
+                    prefixIcon: Icon(Icons.email)),
+                validator: (value) {
+                  if (value == null || value.isEmpty)
+                    return 'Please Enter Some Text';
+                  else {
+                    RegExp regExp = new RegExp(
+                        r'^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$');
+                    if (!regExp.hasMatch(value.toString())) {
+                      return 'Please Enter a valid Email address.';
+                    }
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                style: kTextFieldStyle,
+                decoration: InputDecoration(
+                    labelText: 'Mobile No.',
+                    labelStyle: klabelStyle,
+                    prefixIcon: Icon(Icons.phone)),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please Enter Mobile Number.';
+                  } else if (value.toString().length != 11)
+                    return 'Mobile No. must be equal to 11.';
+                  return null;
+                },
+              ),
+              TextFormField(
+                style: kTextFieldStyle,
+                decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: klabelStyle,
+                    prefixIcon: Icon(Icons.lock)),
+                validator: (value) {
+                  if (value == null || value.isEmpty)
+                    return 'Please Enter Some Text';
+                  else if (value.toString().length < 5)
+                    return 'Password length must be greater than or equal to 5.';
+                  else if (!RegExp(r'^[a-zA-Z0-9]+$')
+                      .hasMatch(value.toString()))
+                    return 'Password must contain only alphabets & digits.';
+                  return null;
+                },
+              ),
+              ListTile(
+                title: ElevatedButton(
+                  onPressed: () {
+                    // Validate returns true if the form is valid, or false otherwise.
+                    if (_formKey.currentState.validate()) {
+                      this._formKey.currentState.save();
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Data Submit.')));
+                    }
+                  },
+                  child: Text('Register'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
